5d5a78765bb9adb7d1ca550e075013175ae56215
rhel8dev <rhel8dev@localhost.localdomain>
2022-05-24 17:41:22 -0300

    bug fix: Make subscriber work

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,7 @@ import {
   Get,
   Logger,
   Post,
+  Query,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
@@ -14,7 +15,8 @@ import {
 } from '@nestjs/microservices';
 import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
 import configuration from './config/configuration';
-import { ConfigService } from "@nestjs/config";
+import { ConfigService } from '@nestjs/config';
+import { Observable } from 'rxjs';
 @Controller('api/v1')
 export class AppController {
   private logger = new Logger(AppController.name);
@@ -25,7 +27,13 @@ export class AppController {
     this.clientAdminBackend = ClientProxyFactory.create({
       transport: Transport.RMQ,
       options: {
-        urls: [`amqp://user:${this.configService.get<string>('RABBITMQ_KEY')}@${this.configService.get<string>('RABBITMQ_HOST')}:${this.configService.get<string>('RABBITMQ_PORT')}/smartranking`],
+        urls: [
+          `amqp://user:${this.configService.get<string>(
+            'RABBITMQ_KEY',
+          )}@${this.configService.get<string>(
+            'RABBITMQ_HOST',
+          )}:${this.configService.get<string>('RABBITMQ_PORT')}/smartranking`,
+        ],
         queue: 'admin-backend',
       },
     });
@@ -33,10 +41,12 @@ export class AppController {
 
   @Post('categorias')
   @UsePipes(ValidationPipe)
-  async criarCategoria(@Body() criarCategoriaDto: CriarCategoriaDto) {
-    return await this.clientAdminBackend.emit(
-      'criar-categoria',
-      criarCategoriaDto,
-    );
+  criarCategoria(@Body() criarCategoriaDto: CriarCategoriaDto) {
+    this.clientAdminBackend.emit('criar-categoria', criarCategoriaDto);
+  }
+
+  @Get('categorias')
+  constultarCategorias(@Query('idCategoria') _id: string): Observable<any> {
+    return this.clientAdminBackend.send('consultar-categorias', _id ? _id : '');
   }
 }
