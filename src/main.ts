62143614eb3d8d707368b8d00570d6a9c048ab49
rhel8dev <rhel8dev@oracle-01.centlinux.com>
2022-06-04 22:29:57 -0300

    Revert "feat: Create logging and timeout interceptors"
    This reverts commit 2c4fbbd85f54bd4e6383757bfd68b5db77765df3.

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './filters/http-exception.filter';
 import * as momentTimezone from 'moment-timezone';
-import { LoggingInterceptor } from './interceptors/logging.interceptor';
-import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.useGlobalInterceptors(new LoggingInterceptor(), new TimeoutInterceptor());
   app.useGlobalFilters(new AllExceptionsFilter());
 
-  Date.prototype.toJSON = function (): any {
+  Date.prototype.toJSON = function(): any {
     return momentTimezone(this)
       .tz('America/Sao_Paulo')
-      .format('YYYY-MM-DD HH:mm:ss.SSS');
-  };
+      .format('YYYY-MM-DD HH:mm:ss.SSS')
+  }
 
   await app.listen(8080);
 }
