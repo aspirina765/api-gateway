ab5f1ee8f86420b63642ff643de40b25feab52ad
rhel8dev <rhel8dev@oracle-01.centlinux.com>
2022-06-05 01:18:11 -0300

    Revert "feat: Add automated tests"
    This reverts commit e73a86f4644ed87795dfe9e1cada20ad8118d4fa.

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,52 +1,21 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Logger,
-  Post,
-  Query,
-  UsePipes,
-  ValidationPipe,
-} from '@nestjs/common';
-import {
-  ClientProxy,
-  ClientProxyFactory,
-  Transport,
-} from '@nestjs/microservices';
-import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
-import configuration from './config/configuration';
-import { ConfigService } from '@nestjs/config';
-import { Observable } from 'rxjs';
-@Controller('api/v1')
-export class AppController {
-  private logger = new Logger(AppController.name);
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
 
-  private clientAdminBackend: ClientProxy;
+describe('AppController', () => {
+  let appController: AppController;
 
-  constructor(private readonly configService: ConfigService) {
-    this.clientAdminBackend = ClientProxyFactory.create({
-      transport: Transport.RMQ,
-      options: {
-        urls: [
-          `amqp://user:${this.configService.get<string>(
-            'RABBITMQ_KEY',
-          )}@${this.configService.get<string>(
-            'RABBITMQ_HOST',
-          )}:${this.configService.get<string>('RABBITMQ_PORT')}/smartranking`,
-        ],
-        queue: 'admin-backend',
-      },
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Hello World!');
     });
-  }
-
-  @Post('categorias')
-  @UsePipes(ValidationPipe)
-  criarCategoria(@Body() criarCategoriaDto: CriarCategoriaDto) {
-    this.clientAdminBackend.emit('criar-categoria', criarCategoriaDto);
-  }
-
-  @Get('categorias')
-  constultarCategorias(@Query('idCategoria') _id: string): Observable<any> {
-    return this.clientAdminBackend.send('consultar-categorias', _id ? _id : '');
-  }
-}
+  });
+});
